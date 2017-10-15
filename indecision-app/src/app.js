c97d49d0b9e2ba7157000de336a3231b1dae2bcd
Vitalii Kushnir <vk@tenios.de>
2017-10-15 14:32:59 +0200

    Hello React

---
+++ indecision-app/src/app.js
@@ -0,0 +1,55 @@
+const app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: []
+};
+
+const onFormSubmit = (e) => {
+    e.preventDefault();
+    const option = e.target.elements.option.value;
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        renderApp();
+    }
+};
+
+const onRemoveAll = () => {
+    app.options = [];
+    renderApp();
+};
+
+const onMakeDecision = () => {
+    const randomNum = Math.floor(Math.random() * app.options.length);
+    const option = app.options[randomNum];
+    alert(option);
+};
+
+const renderApp = () => {
+    const template = (
+        <div>
+            <h1>{app.title}</h1>
+            {app.subtitle && <p>{app.subtitle}</p>}
+            <p>{app.options.length > 0 ? 'Here your options' : 'No options'}</p>
+            <ol>
+                {
+                    app.options.map(item => <li key={item}>{item}</li>)
+                }
+            </ol>
+            <button onClick={onMakeDecision} disabled={app.options.length === 0}>What should I do?</button>
+            <button onClick={onRemoveAll}>Remove All</button>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option"/>
+                <button>Add Option</button>
+            </form>
+        </div>
+    );
+    const appRoot = document.getElementById('app');
+
+    ReactDOM.render(template, appRoot);
+};
+
+renderApp();
+
+
+
