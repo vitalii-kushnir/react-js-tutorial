6d8d89e6b013d48a254b0c6077f49c176e814d35
Vitalii Kushnir <vk@tenios.de>
2017-10-20 12:06:28 +0200

    Using a Third-Party Component

--- indecision-app/src/components/IndecisionApp.js
+++ indecision-app/src/components/IndecisionApp.js
@@ -4,11 +4,13 @@ import AddOption from './AddOption';
 import Options from './Options';
 import Action from './Action';
 import Header from './Header';
+import OptionModal from './OptionModal';
 
 class IndecisionApp extends React.Component {
 
     state = {
-        options: this.props.options
+        options: this.props.options,
+        selectedOption: undefined
     };
 
     componentDidMount(){
@@ -53,10 +55,18 @@ class IndecisionApp extends React.Component {
         this.setState(() => ({options: []}));
     };
 
+    handleClearSelectedOption = () => {
+        this.setState( () => ({
+            selectedOption: undefined
+        }));
+    };
+
     handlePick = () => {
         const randomNum = Math.floor(Math.random() * this.state.options.length);
         const option = this.state.options[randomNum];
-        alert(option);
+        this.setState( (prevState)=> ({
+            selectedOption: option
+        }));
     };
 
     render() {
@@ -69,6 +79,7 @@ class IndecisionApp extends React.Component {
                     handleDeleteOption={this.handleDeleteOption}
                     handleDeleteOptions={this.handleDeleteOptions}/>
                 <AddOption handleAddOption={this.handleAddOption}/>
+                <OptionModal selectedOption={this.state.selectedOption} handleClearSelectedOption={this.handleClearSelectedOption}/>
             </div>
         );
     }
---
+++ indecision-app/src/components/OptionModal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Modal from 'react-modal';
+
+const OptionModal = (props) => (
+    <Modal isOpen={!!props.selectedOption} contentLabel="Selected Option">
+        <h3>Selected Option</h3>
+        {props.selectedOption && <p>{props.selectedOption}</p>}
+        <button onClick={props.handleClearSelectedOption}>OK</button>
+    </Modal>
+);
+
+export default OptionModal;
