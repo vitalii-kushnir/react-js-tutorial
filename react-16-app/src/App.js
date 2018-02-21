e1fe24d061fd28e31047654b5676a9ad47d1cdb5
Vitalii Kushnir <kusha.apple@gmail.com>
2018-02-21 21:19:25 +0100

    react-16-app: Working with Lists and Conditionals

--- react-16-app/src/App.js
+++ react-16-app/src/App.js
@@ -6,59 +6,78 @@ class App extends Component {
 
     state = {
         persons: [{
-            name: 'Vitalii', age: 29
+            name: 'Vitalii', age: 29, id: '1'
         }, {
-            name: 'Jane', age: 26
-        }]
-    };
-
-    switchNameHandler  = (newName) => {
-        this.setState({
-            persons: [{
-                name: 'Vitalii', age: 29
-            }, {
-                name: newName, age: 26
-            }]
-        })
+            name: 'Jane', age: 26, id: '2'
+        }],
+        showPersons: false, id: '3'
     };
 
     /**
      * Example of the two weays binging
      */
-    nameChangedHandler = (e) => {
+    nameChangedHandler = (e, id) => {
         // two ways binding of the components
-        e.preventDefault();
-        this.setState({
-            persons: [{
-                name: 'Vitalii', age: 29
-            }, {
-                name: e.target.value, age: 26
-            }]
-        })
+        const personIdx = this.state.persons.findIndex( p => p.id === id);
+        const person = {
+            ...this.state.persons[personIdx],
+            name: e.target.value
+        };
+
+        const persons = [...this.state.persons];
+        persons[personIdx] = person;
+        this.setState({ persons });
+    };
+
+    togglePersonsHandler = () => {
+        const dowsShow = this.state.showPersons;
+        this.setState({showPersons : !dowsShow});
+    };
+
+    deletePorsonHandler = (idx) => {
+        const persons = [...this.state.persons];
+        persons.splice(idx, 1);
+        this.setState({persons});
     };
 
     render() {
         const style = {
-            backgroundColor : 'white',
+            backgroundColor: 'white',
             font: 'inherit',
             border: '1px solid blue',
             padding: '8px',
             cursor: 'pointer'
         };
+
+        let persons = null;
+
+        // conditional rendering of the content
+        if(this.state.showPersons){
+            persons = (
+              <div>
+                  {
+                      // output lists of elements
+                      this.state.persons.map( (person, idx) => {
+                        return (
+                            <Person key={person.id}
+                                    click =  { () => this.deletePorsonHandler(idx) }
+                                    name={person.name}
+                                    age={person.age}
+                                    changed={(e) => this.nameChangedHandler(e, person.id)}/>
+                        )
+                      })
+                  }
+              </div>
+            );
+        }
+
         return (
             <div className="App">
                 <h1>Hi. I am a React 16 App</h1>
                 <button style={style}
-                    onClick={this.switchNameHandler.bind(this, 'User')}>Switch Name</button>
-                <Person
-                    click={this.switchNameHandler.bind(this, 'Max')}
-                    name={this.state.persons[0].name}
-                    age={this.state.persons[0].age}>My Hobbies: TV</Person>
-                <Person
-                    changed={this.nameChangedHandler}
-                    click={() => this.switchNameHandler('Kusha')}
-                    name={this.state.persons[1].name}
-                    age={this.state.persons[1].age}/>
+                        onClick={this.togglePersonsHandler}>Toggle Persons
+                </button>
+                {persons}
             </div>
         );
     }
